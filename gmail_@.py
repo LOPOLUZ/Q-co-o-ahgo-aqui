392c87af85eb60af4e891e1ad9f37241760e3627
LOPOLUZ <lopoluz48@gmail.com>
2020-04-28 12:59:28 +0200

    Estos archivos han sido eliminados

--- gmail_@.py
+++
@@ -1,8 +0,0 @@
-email=input("Introduce tu gmail: ")
-
-arroba=email.count("@")
-
-if (arroba!=1 or email.find("@")==0 or email.rfind("@")==(len(email)-1)):
-    print("Tu correo es incorreto")
-else:
-    print("Tu correo es correcto")
