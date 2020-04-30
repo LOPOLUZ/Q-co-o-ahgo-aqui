19f41fb84621817b3b6c095a26a6904947404932
LOPOLUZ <lopoluz48@gmail.com>
2020-04-30 12:38:48 +0200

    se ha añadido listas

---
+++ Apuntes/listas.py
@@ -0,0 +1,30 @@
+#a una lista puede estar formada tanto por numero, textos o booleans
+
+mi_lista2=["Francis", 2, True, 4,56]
+print(mi_lista2)
+
+mi_lista=["pepe", "Ramon", "rodri", "adios"]
+print(mi_lista[1])#accede a la posicion marcada 
+
+print(mi_lista[1:3])#accede a las posiciones que estan entre ese rango, contado al primero pero no al ultimo
+
+print(mi_lista[1:])#accede a las posiciones empezando a contar de la posicion que tu ordenes
+
+mi_lista.append("rauul") #Se añade un dato mas al final de la lista
+print(mi_lista) #no abrir corchetes sino se le va a indicar una posicion
+
+mi_lista.insert(1,"Vanesa") #añade un dato mas a la posicion que tu indiques
+print(mi_lista)
+
+mi_lista.extend(["andres", "carlos"]) #añade mas de un elemento a la lista todas las funciones anteriores solo añadian un elemento a la lista
+print(mi_lista)
+
+print(mi_lista.index("rodri")) #para saber en que posicion se encuentra el elemento que marcamos si hay 2 elementos con el mismo nombre nos devolvera el elemento con menor posicion
+
+print("adios" in mi_lista) #le preguntamos si ese elemento esta en mi_lista nos devolvera un valor booleano
+
+mi_lista.remove("rodri", ) # para eliminar un solo elemento de la lista, siempre se debe de ordenar esta funcion antes de imprimir la lista
+print(mi_lista)
+
+mi_lista.pop() #elimina el ultimo elemento de una lista
+print(mi_lista)
